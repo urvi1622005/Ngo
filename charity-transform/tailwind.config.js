0fa66e68dc0cca33b2921728567ea3a5e309aea3
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-10-12 17:56:45 +0530

    vite

---
+++ charity-transform/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          400: '#592720  ',  // Customize the shade of brown
+        },
+      },
+    },
+  },
+  plugins: [],
+};
