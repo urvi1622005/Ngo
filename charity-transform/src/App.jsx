e9c16d451714d756b2cd82ecb12aa0b156d25181
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-10-12 17:57:48 +0530

    app.jsx

---
+++ charity-transform/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Ngo from './components/ngo';
+import Footer from './components/Footer';
+import HomePage from './components/HomePage';
+import Page from './components/Page';
+import Feature from './components/feature'
+import Volunteer from './components/volunteer';
+
+function App() {
+  return (
+    <Router>
+      <Ngo />
+      <Feature/>
+      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<Page />} />
+        
+      </Routes>
+      <Volunteer/>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
