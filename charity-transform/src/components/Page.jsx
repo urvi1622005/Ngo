487d7fdd207374bbbdcaf89d994248bc531d5138
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-10-12 18:01:06 +0530

    page

---
+++ charity-transform/src/components/Page.jsx
@@ -0,0 +1,15 @@
+const AboutPage = () => {
+    return (
+      <div className="min-h-screen bg-gray-100 py-16">
+        <div className="container mx-auto">
+          <h2 className="text-4xl font-bold mb-8">About Us</h2>
+          <p className="text-lg">
+            We are a group of volunteers working towards transforming lives.
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default AboutPage;
+  
