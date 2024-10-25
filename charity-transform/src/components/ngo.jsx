ba81aa1587d3f7a97ade233615456e0975c342ab
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-10-25 17:11:14 +0530

    ..

--- charity-transform/src/components/ngo.jsx
+++ charity-transform/src/components/ngo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
 const Ngo = () => {
@@ -7,13 +8,13 @@ const Ngo = () => {
       {/* Navbar */}
       <nav className="absolute top-0 left-0 w-full bg-transparent text-white z-10">
         <ul className="flex justify-between lg:justify-center items-center space-x-4 lg:space-x-9 py-5 px-4 lg:px-0 border-b border-white/30">
-          <li><a href="#" className="hover:text-gray-500">Home</a></li>
-          <li><a href="#" className="hover:text-gray-500">About</a></li>
-          <li><a href="#" className="hover:text-gray-500">Gallery</a></li>
-          <li><a href="#" className="hover:text-gray-500">Blog</a></li>
-          <li><a href="#" className="hover:text-gray-500">Contact</a></li>
-          <li><a href="#" className="bg-white/25 px-4 py-6 ">Donate</a></li>
-          <li><a href="#" className="hover:text-gray-500">Login</a></li>
+          <li><Link to="/" className="hover:text-gray-500">Home</Link></li>
+          <li><Link to="/about" className="hover:text-gray-500">About</Link></li>
+          <li><Link to="/gallery" className="hover:text-gray-500">Gallery</Link></li>
+          <li><Link to="/blog" className="hover:text-gray-500">Blog</Link></li>
+          <li><Link to="/contact" className="hover:text-gray-500">Contact</Link></li>
+          <li><Link to="/donate" className="bg-white/25 px-4 py-6">Donate</Link></li>
+          <li><Link to="/login" className="hover:text-gray-500">Login</Link></li>
         </ul>
       </nav>
 
