3c038c424516c4610ebece80e2fbeb565cfdea49
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-10-25 17:10:59 +0530

    ..

--- charity-transform/src/App.jsx
+++ charity-transform/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Ngo from './components/ngo';
+import Ngo from './components/Ngo';
 import Footer from './components/Footer';
 import HomePage from './components/HomePage';
 import Page from './components/Page';
-import Feature from './components/feature'
-import Volunteer from './components/volunteer';
-import Recent from './components/recent';
+import Feature from './components/Feature';
+// import Mission from './components/Mission';
+import Volunteer from './components/Volunteer';
+import Recent from './components/Recent';
 import President from './components/President';
 import Content from './components/Content';
 import Donation from './components/Donation';
@@ -13,20 +14,31 @@ import Donation from './components/Donation';
 function App() {
   return (
     <Router>
+      {/* Navbar */}
       <Ngo />
-      
-      
+
+      {/* Main Content */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route 
+          path="/" 
+          element={
+            <HomePage>
+              {/* Adding HomePage sections as children components */}
+              <Recent />
+              <President />
+              <Content />
+              <Donation />
+              {/* <Mission /> */}
+              <Feature />
+              <Volunteer />
+            </HomePage>
+          } 
+        />
         <Route path="/about" element={<Page />} />
-        
+        {/* Add other routes here if needed */}
       </Routes>
-      <Recent/>
-      <President/>
-      <Content/>
-      <Donation/>
-      <Feature/>
-      <Volunteer/>
+
+      {/* Footer */}
       <Footer />
     </Router>
   );
