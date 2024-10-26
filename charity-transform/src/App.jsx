dece68b8bc12ed8b9f93bb8c3ee6fc67fd2163d2
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-10-26 15:39:07 +0530

    ..

--- charity-transform/src/App.jsx
+++ charity-transform/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Ngo from './components/Ngo';
+import Ngo from './components/ngo';
 import Footer from './components/Footer';
 import HomePage from './components/HomePage';
 import Page from './components/Page';
 import Feature from './components/Feature';
-// import Mission from './components/Mission';
+import Mission from './components/Mission';
 import Volunteer from './components/Volunteer';
-import Recent from './components/Recent';
+import Recent from './components/recent';
 import President from './components/President';
 import Content from './components/Content';
 import Donation from './components/Donation';
@@ -28,7 +28,7 @@ function App() {
               <President />
               <Content />
               <Donation />
-              {/* <Mission /> */}
+              <Mission />
               <Feature />
               <Volunteer />
             </HomePage>
