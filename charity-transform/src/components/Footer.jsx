352bfcc44ea93a5e6d8cd58a2edfc1b01fcc845e
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-11-11 14:54:05 +0530

    ..

--- charity-transform/src/components/Footer.jsx
+++ charity-transform/src/components/Footer.jsx
@@ -12,9 +12,9 @@ const Footer = () => {
           {/* Left Section */}
           <div className="space-y-0.5">
             <img
-              src="/src/assets/logo.png" // Replace with actual logo path
+              src="/src/assets/kala.png" // Replace with actual logo path
               alt="Kaladhara Group Logo"
-              className="h-16 w-auto"
+              className="h-30 w-auto"
             />
             <p className="text-gray-400">
               The secret to happiness lies in helping others. Never underestimate
